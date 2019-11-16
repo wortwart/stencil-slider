14be643c06ea2d37d2e3d31984f7bc0a63e6573e
Herbert Braun <wortwart@woerter.de>
2019-11-16 23:23:44 +0100

    Add JSX markup

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -6,11 +6,18 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true
 })
 export class Slider {
+	slide(amount: number = 1) {
+		console.log(amount);
+	}
 
   render() {
     return (
       <Host>
-        <slot></slot>
+				<button type="button" class="control control_next" onClick={this.slide.bind(this, 1)}>&gt;</button>
+				<button type="button" class="control control_prev" onClick={this.slide.bind(this, -1)}>&lt;</button>
+				<ul>
+					<slot/>
+				</ul>
       </Host>
     );
   }
