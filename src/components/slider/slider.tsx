4e23c85f6fe60a500b8ff4534cb6a72eef592532
Herbert Braun <wortwart@woerter.de>
2019-11-20 22:52:42 +0100

    Simplify markup and SCSS

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -13,11 +13,12 @@ export class Slider {
   render() {
     return (
       <figure>
-				<button type="button" class="control control_next" onClick={this.slide.bind(this, 1)}>&gt;</button>
-				<button type="button" class="control control_prev" onClick={this.slide.bind(this, -1)}>&lt;</button>
+				<button type="button" class="btn_next" onClick={this.slide.bind(this, 1)}>&gt;</button>
+				<button type="button" class="btn_prev" onClick={this.slide.bind(this, -1)}>&lt;</button>
 				<ul>
 					<slot/>
 				</ul>
+				<p>Test!</p>
       </figure>
     );
   }
